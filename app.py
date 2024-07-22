e1de9645c96f03f265f8af866ff2d880df13a76a
Johnathon Crowder <139363360+JohnathonCrowder@users.noreply.github.com>
2024-07-22 10:33:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 import pandas as pd
 import folium
-from geopy.distance import geodesic
+from geopy import geodesic
 import geocoder
 
 app = Flask(__name__)
